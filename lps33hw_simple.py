beab5f2a9fdec286cb8618705b90491c652f1ee1
OlivierdenOuden <38492038+OlivierdenOuden@users.noreply.github.com>
2018-11-30 13:29:15 +0100

    Update lps33hw_simple.py

--- lps33hw_simple.py
+++ lps33hw_simple.py
@@ -1,14 +1,14 @@
 #*******************************************************#
-#														#
-#		LPS33HW - ST Electronics Pressure sensor		#
-#														#
-#		Simple Read-out script.							#
-#														#
-#		Olivier den Ouden								#
-#		Royal Netherlands Meterological Institute		#
-#		RD Seismology and Acoustics						#
-#		https://www.seabirdsound.org 					#
-#														#
+#														                            #
+#		LPS33HW - ST Electronics Pressure sensor	        	#
+#													                            	#
+#		Simple Read-out script.			                				#
+#												                            		#
+#		Olivier den Ouden			                    					#
+#		Royal Netherlands Meterological Institute		        #
+#		RD Seismology and Acoustics				              		#
+#		https://www.seabirdsound.org 			              		#
+#												                            		#
 #*******************************************************#
 
 # Modules
